78389775049cfec05301793106bfd62923da125a
hugosandsjo <hugosandsjo@gmail.com>
2024-03-03 14:08:13 +0100

    UpdateBookingTest up and runnning

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,16 @@ use App\Http\Controllers\ViewBookingsController;
 
 Route::view('/', 'index')->name('login');
 
-Route::patch('/bookings/{booking}', UpdateBookingController::class);
+Route::patch('/bookings/{booking}', UpdateBookingController::class)->name('booking.update');
 
 Route::post('login', LoginController::class);
 Route::post('bookings', CreateBookingController::class);
+Route::post('/view-bookings', ViewBookingsController::class);
 
 Route::get('dashboard', DashboardController::class);
 Route::get('/logout', LogoutController::class);
+Route::get('/search-cars', [SearchCars::class, 'searchCars'])->name('cars.search');
 
 Route::delete('bookings/{booking}/delete', DeleteBookingController::class)->name('booking.destroy');
-
-Route::post('/view-bookings', ViewBookingsController::class);
-
-Route::get('/search-cars', [SearchCars::class, 'searchCars'])->name('cars.search');
 
 // Unused?
