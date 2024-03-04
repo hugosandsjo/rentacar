358996fc12271067b007868635ff438af240aab9
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2024-03-04 15:10:18 +0100

    switch between forms

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,41 +4,101 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <style>
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 60px;
+            height: 34px;
+        }
+
+        .switch input {
+            display: none;
+        }
+
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 26px;
+            width: 26px;
+            left: 4px;
+            bottom: 4px;
+            background-color: white;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+
+        input:checked+.slider {
+            background-color: #2196F3;
+        }
+
+        input:focus+.slider {
+            box-shadow: 0 0 1px #2196F3;
+        }
+
+        input:checked+.slider:before {
+            -webkit-transform: translateX(26px);
+            -ms-transform: translateX(26px);
+            transform: translateX(26px);
+        }
+
+        .slider.round {
+            border-radius: 34px;
+        }
+
+        .slider.round:before {
+            border-radius: 50%;
+        }
+    </style>
 </head>
 
 <body>
+
+
     <h1>Rentacar</h1>
-<div class="login-div">
-
-    <form method="post" action="/login">
-        @csrf
-        <div>
-            <label for="email">Email</label>
-            <input name="email" id="email" type="email" />
+    <div class="login-div">
+        <div class="form-switch">
+            <p>Login</p>
+            <label class="switch">
+                <input type="checkbox" id="viewToggle">
+                <span class="slider round"></span>
+            </label>
+            <p>Sign up</p>
         </div>
-        <div>
-            <label for="password">Password</label> 
-            <input name="password" id="password" type="password" />
-        </div>
-        <label for="remember">Remember Me:</label>
-        <input type="checkbox" id="remember" name="remember">
+        <div id="content"></div>
+    </div>
+    <script>
+        window.onload = function() {
+            fetch('/login')
+                .then(response => response.text())
+                .then(html => {
+                    document.getElementById('content').innerHTML = html;
+                });
+        };
 
-        <button type="submit">Login</button>
-    </form>
+        document.getElementById('viewToggle').addEventListener('change', function() {
+            const viewToFetch = this.checked ? 'signup' : 'login';
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-</div>
-
-
+            fetch(`/${viewToFetch}`)
+                .then(response => response.text())
+                .then(html => {
+                    document.getElementById('content').innerHTML = html;
+                });
+        });
+    </script>
 </body>
 
 </html>
--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,29 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+<h2>Sign up</h2>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+<form method="POST" action="{{ route('createuser') }}">
+    @csrf
 
-<body>
-    <form method="POST" action="{{ route('createuser') }}">
-        @csrf
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name" required>
 
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" required>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
+    <button type="submit">Register</button>
+</form>
 
-        <button type="submit">Register</button>
-    </form>
 
-</body>
 
-</html>
+
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
