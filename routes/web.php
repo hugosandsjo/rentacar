a7b9751991bc26b6edcc6acd9d80e68988971aea
hugosandsjo <hugosandsjo@gmail.com>
2024-03-06 13:44:32 +0100

    completed test

--- routes/web.php
+++ routes/web.php
@@ -19,28 +19,27 @@ Route::view('/', 'index')->name('login');
 
 
 
-Route::patch('/bookings/{booking}', UpdateBookingController::class)->name('booking.update')->middleware('auth');
+Route::patch('/bookings/{booking}', UpdateBookingController::class)->name('booking.update')->middleware('auth'); //test done
 
-Route::post('login', LoginController::class);
-
-Route::post('/createuser', CreateUser::class)->name('createuser');
-Route::post('bookings', CreateBookingController::class)->middleware('auth');
-Route::post('/view-bookings', ViewBookingsController::class)->middleware('auth');
+Route::post('login', LoginController::class); //test done
+Route::post('/createuser', CreateUser::class)->name('createuser'); //test done
+Route::post('bookings', CreateBookingController::class)->middleware('auth'); //test done
+Route::post('/view-bookings', ViewBookingsController::class)->middleware('auth'); //test done
 
 
 // Route::get('signup', Signup::class);
-Route::get('dashboard', DashboardController::class)->middleware('auth');
-Route::get('/logout', LogoutController::class);
-Route::get('/search-cars', [SearchCars::class, 'searchCars'])->name('cars.search');
+Route::get('dashboard', DashboardController::class)->middleware('auth'); //test done
+Route::get('/logout', LogoutController::class); //test done
+Route::get('/search-cars', [SearchCars::class, 'searchCars'])->name('cars.search'); //test done
 
 Route::get('/login', function () {
     return view('login');
-})->name('login');
+})->name('login'); //test done
 
 Route::get('/signup', function () {
     return view('signup');
-})->name('signup');
+})->name('signup'); //test done
 
-Route::delete('bookings/{booking}/delete', DeleteBookingController::class)->name('booking.destroy')->middleware('auth');;
+Route::delete('bookings/{booking}/delete', DeleteBookingController::class)->name('booking.destroy')->middleware('auth');; //test done
 
 // Unused?
